c0eaef0f08ab51dd9166e43d158699d155831598
KrisMoe <moe00013@KrisLaptop>
2024-02-13 20:57:44 -0600

    finnished the playlist page

--- server.py
+++ server.py
@@ -11,24 +11,23 @@ def layout():
 @app.route('/homepage', methods=['GET'])
 def homepage():
   return render_template('homepage.html.jinja',user_id = 0, playlists = [{'image':'image goes here','name':'playlist name goes here','rating':'rating goes here','tags':['tag1','tag2','tag3']}])
-'''
+
 @app.route('/search', methods=['POST'])
 def search():
   return render_template('search.html')
 
-@app.route('/playlist/<int:p_id>', methods=['POST'])
+@app.route('/playlist/<int:p_id>', methods=['POST','GET'])
 def playlist(p_id):
-  return render_template('playlist.html', playlist_id=p_id)
+  return render_template('playlist.html.jinja', playlist_id=p_id,songs= ["Minnesota March","Minnesota Rouser"],comments= ["Lovely","good vibes"])
 
 @app.route('/login', methods=['GET'])
 def login():
-  return render_template('login.html')
+  return render_template('login.html.jinja')
 
 @app.route('/settings', methods=['GET'])
 def settings():
-  return render_template('settings.html')
+  return render_template('settings.html.jinja')
 
 @app.route('/library/<int:u_id>', methods=['POST'])
 def library(u_id):
-  return render_template('library.html', user_id=u_id)
-'''
+  return render_template('library.html.jinja', user_id=u_id)
