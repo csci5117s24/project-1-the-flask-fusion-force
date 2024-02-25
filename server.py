b38ec8d259b9328d917184100279efc54420fd55
KrisMoe <moe00013@KrisLaptop>
2024-02-24 23:34:21 -0600

    logout/login checking added to html

--- server.py
+++ server.py
@@ -19,27 +19,28 @@ app = create_app()
 @app.route('/home', methods=['GET'])
 @app.route('/homepage', methods=['GET'])
 def homepage():
-  return render_template('homepage.html.jinja',user_id = 0,session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4),
-   playlists = [{'image':'image goes here','name':'playlist name goes here','rating':'rating goes here','tags':['tag1','tag2','tag3']}])
+    print(session.get('user'))
+    return render_template('homepage.html.jinja',user_id=session.get('user'), pretty=json.dumps(session.get('user'), indent=4),
+    playlists = [{'image':'image goes here','name':'playlist name goes here','rating':'rating goes here','tags':['tag1','tag2','tag3']}])
 
 @app.route('/search', methods=['POST','GET'])
 def search():
-  return render_template('search.html.jinja',user_id = session.get('user'), playlists = [{'image':'image goes here','name':'playlist name goes here','rating':'rating goes here','tags':['tag1','tag2','tag3']}])
+    return render_template('search.html.jinja',user_id = session.get('user'), playlists = [{'image':'image goes here','name':'playlist name goes here','rating':'rating goes here','tags':['tag1','tag2','tag3']}])
 @app.route('/playlist/<int:p_id>', methods=['POST','GET'])
 def playlist(p_id):
-  return render_template('playlist.html.jinja', playlist_id=p_id,songs= ["Minnesota March","Minnesota Rouser"],comments= ["Lovely","good vibes"])
+    return render_template('playlist.html.jinja', playlist_id=p_id,user_id = session.get('user'),songs= ["Minnesota March","Minnesota Rouser"],comments= ["Lovely","good vibes"])
 @app.route('/settings', methods=['GET'])
 def settings():
-  return render_template('settings.html.jinja',settings = 
+    return render_template('settings.html.jinja',settings = 
   {"text example":["text"],"show me a cat":["upload"],"Fruits":["dropdown",["oranges","option2"]],"Toggel example":["checkbox"]})
 
 @app.route('/library', methods=['POST','GET'])
 def library():
-  return render_template('user_library.html.jinja', user_id=session.get('user'))
+    return render_template('user_library.html.jinja', user_id=session.get('user'))
 
 @app.route('/edit-playlist', methods=['POST','GET'])
 def editplaylist():
-  return render_template('create_edit_playlist.html.jinja', user_id=session.get('user'),searched_songs= ["Minnesota March","Minnesota Rouser"])
+    return render_template('create_edit_playlist.html.jinja', user_id=session.get('user'),searched_songs= ["Minnesota March","Minnesota Rouser"])
 
 # @app.route('/rate-playlist', methods=['POST'])
 def ratePlaylist():
@@ -102,7 +103,7 @@ def logout():
         + "/v2/logout?"
         + urlencode(
             {
-                "returnTo": url_for("home", _external=True),
+                "returnTo": url_for("homepage", _external=True),
                 "client_id": env.get("AUTH0_CLIENT_ID"),
             },
             quote_via=quote_plus,
