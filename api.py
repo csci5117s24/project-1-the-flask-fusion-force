37b88d53b82a3636944f8001ece1092772b330aa
AlienIdeology <wcliaw610@gmail.com>
2024-02-26 13:46:17 -0600

    all pure api routes except unsave playlist is functional

--- api.py
+++ api.py
@@ -1,38 +1,84 @@
+import traceback
 from flask import Blueprint, request, Response
 import db
 
-app = Blueprint('auth', __name__)
+app = Blueprint('api', __name__)
 
-@app.route("/playlist/save", method=['POST'])
+def wrapReqChecking(f):
+    try:
+        ret = f()
+    except Exception as e:
+        print(traceback.format_exc())
+        return Response(status=400, response=getattr(e, 'message', repr(e)), mimetype="text/plain")
+    else:
+        return ret
+
+def wrapDBFunc(f):
+    try:
+        f()
+    except Exception as e:
+        print(traceback.format_exc())
+        return Response(status=500, response=getattr(e, 'message', repr(e)), mimetype="text/plain")
+    else:
+        return Response(status=201, response='test', mimetype="text/plain")
+
+@app.route("/playlist/save", methods=['POST'])
 def save_playlist():
-    user_id = request.args.get('userID')
-    playlist_id = request.args.get('playlistID')
+    def f():
+        json = request.get_json()
+        user_id = json['userID']
+        playlist_id = json['playlistID']
+        return wrapDBFunc(lambda: db.savePlaylist(user_id, playlist_id))
+    return wrapReqChecking(f)
 
-    db.savePlaylist(user_id, playlist_id)
-    return Response(status=201)
-
-@app.route("/playlist/unsave", method=['POST'])
+@app.route("/playlist/unsave", methods=['POST'])
 def unsave_playlist():
-    user_id = request.args.get('userID')
-    playlist_id = request.args.get('playlistID')
+    def f():
+        json = request.get_json()
+        user_id = json['userID']
+        playlist_id = json['playlistID']
+        #return wrapDBFunc(lambda: db.unsavePlaylist(user_id, playlist_id))
+    return wrapReqChecking(f)
 
-    # db.unsavePlaylist(user_id, playlist_id)
-    return Response(status=201)
-
-@app.route("/playlist/unsave", method=['POST'])
+@app.route("/playlist/unsave", methods=['POST'])
 def rate_playlist():
-    user_id = request.args.get('userID')
-    playlist_id = request.args.get('playlistID')
-    rating = request.args.get('rating')
+    def f():
+        json = request.get_json()
+        user_id = json['userID']
+        playlist_id = json['playlistID']
+        rating = request.args.get('rating')
+        return wrapDBFunc(lambda: db.ratePlaylist(user_id, playlist_id, rating))
+    return wrapReqChecking(f)
 
-    db.ratePlaylist(user_id, playlist_id, rating)
-    return Response(status=201)
-
-@app.route("/playlist/comment/add", method=['POST'])
+@app.route("/playlist/comment/add", methods=['POST'])
 def add_comment():
-    user_id = request.args.get('userID')
-    playlist_id = request.args.get('playlistID')
-    commentText = request.args.get('commentText')
+    def f():
+        json = request.get_json()
+        user_id = json['userID']
+        playlist_id = json['playlistID']
+        commentText = json['commentText']
+        return wrapDBFunc(lambda: db.addComment(user_id, playlist_id, commentText))
+    return wrapReqChecking(f)
 
-    db.addComment(user_id, playlist_id, commentText)
-    return Response(status=201)
+@app.route("/playlist/update", methods=['POST'])
+def update_playlist():
+    def f():
+        json = request.get_json()
+        user_id = json['userID']
+        playlist_id = json['playlistID']
+        song_ids = json['songIDs']
+        playlist_name = json['playlistName']
+        playlist_image = json['playlistImage']
+        return wrapDBFunc(lambda: db.updatePlaylist(user_id, playlist_id, song_ids, playlist_name, playlist_image))
+    return wrapReqChecking(f)
+
+@app.route("/playlist/create", methods=['POST'])
+def create_playlist():
+    def f():
+        json = request.get_json()
+        user_id = json['userID']
+        playlist_name = json['playlistName']
+        playlist_image = json['playlistImage']
+        song_ids = json['songIDs']
+        return wrapDBFunc(lambda: db.createPlaylist(user_id, playlist_name, playlist_image, song_ids))
+    return wrapReqChecking(f)
